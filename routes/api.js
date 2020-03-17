19317d658254cad4daaf79be5597b5d1113f58a4
Ferry Chavez <ferry.chavez@gmail.com>
2020-03-16 17:09:36 -0700

    commit

---
+++ routes/api.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/transaction", async ({ body }, res) => {
+  try {
+    const dbTransaction = await Transaction.create(body)
+    res.json(dbTransaction);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/api/transaction/bulk", async ({ body }, res) => {
+  try {
+    const dbTransaction = await Transaction.insertMany(body)
+    res.json(dbTransaction);
+  } catch (err) {
+    res.status(400).json(err);
+  };
+});
+
+router.get("/api/transaction", async (req, res) => {
+  try {
+    const dbTransaction = await Transaction.find({}).sort({ date: -1 })
+    res.json(dbTransaction);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
